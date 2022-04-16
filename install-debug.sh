1afc56669edb26548ae4cc0a70e279027141acd5
0x000000000019b6 <77126080+0x000000000019b6@users.noreply.github.com>
2022-04-16 08:46:07 +0200

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -375,7 +375,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/0x000000000019b6/yiimp_pool.git yiimp
+    git clone https://github.com/npq7721/gr_pool.git yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     sudo make
